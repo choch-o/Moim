de2700ba3275cb46c56cdea1874486db4231d72e
hyunsungcho <hscho1226@gmail.com>
2017-01-16 17:30:46 +0900

    Attendance check button changes with states

--- app/Attendance.js
+++ app/Attendance.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { View, Text, TouchableHighlight } from 'react-native';
+import { View, Text, TouchableHighlight, Button } from 'react-native';
 
 import GeolocationExample from './GeolocationExample'
 
@@ -16,8 +16,19 @@ export default class Attendance extends Component {
   
     this.state = {
       initialPosition: 'unknown',
-      lastPosition: 'unknown'
+      lastPosition: 'unknown',
+      currentLatitude: 'unknown',
+      currentLongitude: 'unknown',
+      targetLatitude: 'unknown',
+      targetLongitude: 'unknown',
+      isNearby: false
     };
+    this.getTargetLocation()
+      .then((location) => {
+        this.setState({targetLatitude: location.latitude})
+        this.setState({targetLongitude: location.longitude})
+      })
+    this.onCheckPressed = this.onCheckPressed.bind(this)
   }
 
   watchID = (null: ?number);
@@ -34,29 +45,90 @@ export default class Attendance extends Component {
     this.watchID = navigator.geolocation.watchPosition((position) => {
       var lastPosition = JSON.stringify(position);
       this.setState({lastPosition});
-    });
+      this.setState({currentLatitude: position.coords.latitude})
+      this.setState({currentLongitude: position.coords.longitude})
+    });    
+    
   }
 
   componentWillUnmount = () => {
     navigator.geolocation.clearWatch(this.watchID);
   }
 
+  getTargetLocation() {
+    return fetch('http://52.78.52.132:3000/attendance/location/n1')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        return responseJson
+      })
+      .catch((error) => {
+        console.error(error)
+      });
+  }
+
+  onCheckPressed() {
+    if (this.state.isNearby) {
+      if (this.state.checked != true) {
+        this.setState({ checked: true })
+      }
+    }
+  }
+
+  componentDidUpdate() {
+    var currLat = parseFloat(this.state.currentLatitude).toFixed(3)
+    var currLong = parseFloat(this.state.currentLongitude).toFixed(3)
+    var targetLat = parseFloat(this.state.targetLatitude).toFixed(3)
+    var targetLong = parseFloat(this.state.targetLongitude).toFixed(3)
+
+    console.log(currLat)
+    console.log(currLong)
+    console.log(targetLat)
+    console.log(targetLong)
+
+    if (!this.state.isNearby) {
+      if (!isNaN(currLat) && !isNaN(currLong) && !isNaN(targetLat) && !isNaN(targetLong)) {
+        if ((currLat == targetLat) && (currLong == targetLong)) {
+          console.log("SET TRUE")
+          this.setState({isNearby: true})
+        }
+      } 
+    }
+  }
+
+
   render() {
+    const checkStyle = {
+      flex: 8,
+      backgroundColor: 'pink',
+      alignSelf: 'stretch',
+      justifyContent: 'center',
+      marginLeft: 50,
+      marginRight: 50,
+      marginTop: 200,
+      marginBottom: 200
+    }
+    var checkButton;
+    if (this.state.isNearby) {
+      if (this.state.checked) {
+        checkButton = <Text style={{ textAlign: 'center' }}>CHECKED</Text>
+      }
+      else {
+        checkButton = <Button title='CHECK' onPress={ this.onCheckPressed }></Button>
+      }
+    } else {
+      checkButton = <Text style={{ textAlign: 'center' }}>NOT AVAILABLE</Text>
+    }
+
     return (
-      <View>
+      <View style={{ flex: 1, flexDirection: 'column' }} >
         <TouchableHighlight
-          style={{ width: 50, height: 50 }}
+          style={{ flex: 1 }}
           onPress={ () => this.props.navigator.pop() } >
           <Text style={{ padding: 40 }}>Back</Text>
         </TouchableHighlight>
-        <Text>
-          <Text>Initial position: </Text>
-          {this.state.initialPosition}
-        </Text>
-        <Text>
-          <Text>Current position: </Text>
-          {this.state.lastPosition}
-        </Text> 
+        <View style={ checkStyle }>
+          { checkButton }
+        </View>
       </View>     
     );
   }
