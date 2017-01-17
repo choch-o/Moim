bd55793eb01f8500058450c709c4247de0658587
choch-o <hscho1226@gmail.com>
2017-01-17 22:58:43 +0000

    fixed tab

--- react-project/src/meeting_board.js
+++ react-project/src/meeting_board.js
@@ -4,79 +4,79 @@ import { Link } from 'react-router';
 
 export default class meeting_board extends React.Component{
 
-	constructor(props) {
-		    
-		super(props);
-		this.state = {	
-			meetings: []};
-		this.componentWillMount = this.componentWillMount.bind(this);
+  constructor(props) {
 
-	}
+    super(props);
+    this.state = {
+      meetings: []};
+    this.componentWillMount = this.componentWillMount.bind(this);
 
-	componentWillMount(){
-		axios.post('/call_meeting_board',{
-		}).then(function(response){
-			var nextState={};
-			console.log(response.data);
-			nextState['meetings'] = response.data;
-			this.setState(nextState);		
-		}.bind(this))
-		.catch(function(response) {
-			console.log(response);
-		});
+  }
 
-	}
+  componentWillMount(){
+    axios.post('/call_meeting_board',{
+    }).then(function(response){
+      var nextState={};
+      console.log(response.data);
+      nextState['meetings'] = response.data;
+      this.setState(nextState);
+    }.bind(this))
+      .catch(function(response) {
+        console.log(response);
+      });
 
-	render(){
-		console.log(this.state.meetings);
-		return (
-			<div>
-				<ul>
-					{this.state.meetings.map(
-									
-							function(given_meeting) {							                       
-							
-								return (<MeetingInfo meeting={given_meeting}/>);
-					
-							})}
-	
-				</ul>
-			</div>
-		);
-	}
+  }
+
+  render(){
+    console.log(this.state.meetings);
+    return (
+      <div>
+        <ul>
+          {this.state.meetings.map(
+
+            function(given_meeting) {
+
+              return (<MeetingInfo meeting={given_meeting}/>);
+
+            })}
+
+          </ul>
+        </div>
+    );
+  }
 
 }
 
 class MeetingInfo extends React.Component {
 
 
-	constructor(props){
-		super(props);
-		this.state={
-			presentor: this.props.meeting['presentor'],
-			meeting_title: this.props.meeting['meeting_name'],
-			meeting_date: this.props.meeting['meeting_date'],
-			id: this.props.meeting['_id']
-		}	
-		console.log(this.state.id);
-	}
+  constructor(props){
+    super(props);
+    this.state={
+      presentor: this.props.meeting['presentor'],
+      meeting_title: this.props.meeting['meeting_name'],
+      meeting_date: this.props.meeting['meeting_date'],
+      id: this.props.meeting['_id']
+    }
+    console.log(this.state.id);
+  }
 
 
-	render() {
+  render() {
 
-		return(
-				<Link to={"meeting/"+this.state.id}>
-					<div>		
-						<span>presentor: {this.state.presentor}&nbsp;</span>	
-						<span>title: {this.state.meeting_title}&nbsp;</span>
-						<span>date: {this.state.meeting_date}&nbsp;</span>
-					</div>
-				</Link>
+    return(
+      <Link to={"meeting/"+this.state.id}>
+        <div>
+          <span>presentor: {this.state.presentor}&nbsp;</span>
+          <span>title: {this.state.meeting_title}&nbsp;</span>
+          <span>date: {this.state.meeting_date}&nbsp;</span>
+        </div>
+      </Link>
 
 
-			  );
-	
-	}
+    );
+
+  }
 }
 
 
--- react-project/src/meeting_wrapper.js
+++ react-project/src/meeting_wrapper.js
@@ -4,15 +4,15 @@ import styles from './meeting_header.css';
 
 
 const Meeting_wrapper = ({children, date}) => {
-	    return (
-				        
-				<div className="meeting_wrapper">
-					<div className={styles.Header}>{date}</div>
-					{children}
+  return (
 
-				</div>
+    <div className="meeting_wrapper">
+      <div className={styles.Header}>{date}</div>
+      {children}
 
-			   );
+    </div>
+
+  );
 };
- 
+
 export default Meeting_wrapper;
